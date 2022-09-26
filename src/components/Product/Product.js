7f621ba4eda5815c497e689a7acde5e655f41ba6
Amit Sarker <amitsarker255@gmail.com>
2022-09-26 06:24:05 +0600

    first commit

---
+++ src/components/Product/Product.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+
+import "./Product.css";
+const Product = (props) => {
+  //   console.log(props);
+
+  const { img, name, seller, price, ratings } = props.product;
+
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <h3>{name}</h3>
+        <h4>Price : ${price}</h4>
+        <p>Manufacturer : {seller}</p>
+        <p>Rating : {ratings} stars</p>
+      </div>
+      <button
+        onClick={() => props.handleAddToCart(props.product)}
+        className="btn-card"
+      >
+        <p className="btn-text">Add To Cart</p>
+        <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+      </button>
+    </div>
+  );
+};
+
+export default Product;
